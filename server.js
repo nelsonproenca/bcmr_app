a11de027481ca779f61697e9d4aca9e9b1e00c71
nproenca <nproenca@whitestar.pt>
2021-09-05 01:01:13 -0300

    Novo server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
-    return res.redirect("https://" + req.get("host") + req.url);
-  }
-  next();
-}
-
 const express = require("express");
+const path = require("path");
+const nomeApp = "bcmr-app";
 const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static("./dist/bcmr-app"));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/bcmr-app/" });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
+
+app.listen(process.env.PORT || 8080);
